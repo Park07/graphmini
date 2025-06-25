2692c96f5438b8ac6c877c80a670e01b10f73da3
William Park <galaxiapark07@gmail.com>
2025-06-25 22:53:10 +1000

    cleaning up and removing redundant files

--- src/codegen_output/plan.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../backend/backend.h"
-namespace minigraph
-{
-    using GraphType = Graph;
-    using VertexSetType = VertexSet;
-    void plan(const GraphType* graph, Context& ctx);
-    uint64_t pattern_size();
-}
