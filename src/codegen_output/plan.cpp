a1993e178ca302e949dc14f9f4bb5abb5677a400
William Park <galaxiapark07@gmail.com>
2025-07-28 06:16:12 +1000

    SAVE: about to modify the test script + medium

--- src/codegen_output/plan.cpp
+++ src/codegen_output/plan.cpp
@@ -1,6 +1,6 @@
 #include "plan.h"
 namespace minigraph {
-uint64_t pattern_size() { return 4; }
+uint64_t pattern_size() { return 3; }
 void plan(const GraphType *graph, Context &ctx) {
   using MiniGraphType = MiniGraphCostModel;
   MiniGraphIF::DATA_GRAPH = graph;
@@ -19,25 +19,11 @@ void plan(const GraphType *graph, Context &ctx) {
       if (s0.size() == 0)
         continue;
       /* VSet(0, 0) In-Edges: 0 Restricts: 0 */
-      MiniGraphEager m0(true, false);
-      /* Vertices = VSet(0) In-Edges: 0 Restricts: 0  | Intersect = VSet(0)
-       * In-Edges: 0 Restricts: 0 */
-      m0.build(s0, s0, s0);
-      // skip building indices for m0 because they can be obtained directly
       for (size_t i1_idx = 0; i1_idx < s0.size(); i1_idx++) { // loop-1 begin
         const IdType i1_id = s0[i1_idx];
-        VertexSet m0_adj = m0.N(i1_idx);
-        VertexSet s1 = m0_adj.bounded(i1_id);
-        if (s1.size() == 0)
-          continue;
+        VertexSet i1_adj = graph->N(i1_id);
+        counter += s0.intersect_cnt(i1_adj, i1_adj.vid());
         /* VSet(1, 1) In-Edges: 0 1 Restricts: 0 1 */
-        auto m0_s1 = m0.indices(s1);
-        for (size_t i2_idx = 0; i2_idx < s1.size(); i2_idx++) { // loop-2 begin
-          const IdType i2_id = s1[i2_idx];
-          VertexSet m0_adj = m0.N(m0_s1[i2_idx]);
-          counter += s1.intersect_cnt(m0_adj, m0_adj.vid());
-          /* VSet(2, 2) In-Edges: 0 1 2 Restricts: 0 1 2 */
-        } // loop-2 end
       } // loop-1 end
       handled += 1;
     } // loop-0 end
